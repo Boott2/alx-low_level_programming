cd080b0b4295c357228a5794dc2943e38d9697d8
Mounir Serbout <mounirserbout@hotmail.com>
2023-06-30 15:25:20 -0700

    Print list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdarg.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: the string
+ * @len: the lenght of the array
+ * @next: pointer to the next node
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
